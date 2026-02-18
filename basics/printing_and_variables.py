679970ea051d8f00d7a91c6d99a593ede0739c31
Ark-tHe-sHaRk <arkbarot8812coder@gmail.com>
2026-02-18 11:09:45 +0000

    Printing and variables and a small project

---
+++ basics/printing_and_variables.py
@@ -0,0 +1,14 @@
+#You can declare a varible by giving it a name and then an equal sign next to it to assign a value
+name = "Ark"
+age = 13
+goal = "Cybersecurity and AI"
+
+#You can print a varibles value by simply using the print satement and then putting your varible name into the brackets without quotation marks
+print("My name is", name)
+print("I am", age, "years old right now when I have made this")
+print("My goal is to work in", goal)
+
+#You can also change varibles
+Age = age + 1
+print("Next year I will be", Age, "years old")
+
--- basics/variables.py
+++
@@ -1,7 +0,0 @@
-name = "Ark"
-age = 13
-goal = "Cybersecurity and AI"
-
-print("My name is", name)
-print("I am", age, "years old right now when I have made this")
-print("My goal is to work in", goal)
