affdcad3ea1eeffb1af2643cf2e122792a16509e
Ark-8812 <arkbarot8812coder@gmail.com>
2026-02-27 17:55:59 +0000

    Enhance 05_Loops.py with loop explanations
    Added explanations and examples for while and for loops.

--- basics/05_Loops.py
+++ basics/05_Loops.py
@@ -1,1 +1,14 @@
+#Loops allow your code to keep running or on repeat without writing it many times
 
+#A "while" loop allows the code to run while the condition is still true
+
+count = 1 
+
+while count  <= 5:
+  print("Count is ", count)
+  count = count + 1     #Increases count each time
+
+#A "for" loop repeats a set number of times
+
+for i in range(10):
+  print("This is loop number", i) #You can use any letter no need to just put I
