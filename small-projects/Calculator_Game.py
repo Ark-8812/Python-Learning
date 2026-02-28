0d8ca7b5f31013d40b2c680dc91c0abe352f14ca
Ark-8812 <arkbarot8812coder@gmail.com>
2026-02-28 09:34:35 +0000

    Update Calculator_Game.py

--- small-projects/Calculator_Game.py
+++ small-projects/Calculator_Game.py
@@ -1,1 +1,10 @@
+print("Simple Calculator")
 
+#Creating 2 inputs for the user 
+num1 = float(input("What is your first number: "))  
+num2 = float(input("What is your second number: "))
+
+print("Choose a operation:")
+print("1. Add")
+print("2. Subtract")
+print("3.")
