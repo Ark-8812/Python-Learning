ecf258bb6414b870a37312239582f7a3e3ebc459
Ark-8812 <arkbarot8812coder@gmail.com>
2026-03-04 17:50:22 +0000

    Add multiplication and division options to calculator

--- small-projects/Calculator_Game.py
+++ small-projects/Calculator_Game.py
@@ -7,4 +7,18 @@ num2 = float(input("What is your second number: "))
 print("Choose a operation:")
 print("1. Add")
 print("2. Subtract")
-print("3.")
+print("3. Multiply")
+print("4. Divide")
+
+choice = int(input("Enter your choice (1-4)"))
+
+if choice == 1:
+    print("Result:", num1 + num2)
+elif choice == 2:
+    print("Result:", num1 - num2)
+elif choice == 3:
+    print("Result:", num1 * num2)
+elif choice == 4:
+    print("Result:", num1 / num2)
+else:
+    print("Invalid choice")
